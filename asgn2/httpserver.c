a7b0838fc4ad97cb28358c9aacab00ae9c4fbc0f
Reema <rechinta@ucsc.edu>
2024-02-07 18:36:17 +0000

    echo server almost working

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -3,6 +3,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
 #include "asgn2_helper_funcs.h"
 
 int main(int argc, char* argv[]){
@@ -27,22 +29,28 @@ int main(int argc, char* argv[]){
         exit(1);
     }
 
-    //while (1){
+    
+
+    while (1){
         sock = listener_accept(socket);
 
-        int buff_size = 10000;
+        int buff_size = 2048;
+        char buffer[buff_size];
 
-         char buffer[buff_size];
+        ssize_t bytes_read = 0;
 
-        ssize_t bytes_read = read_n_bytes(sock, buffer, buff_size);
+        do{
+            bytes_read = read_n_bytes(sock, buffer, 5);
+            ssize_t bytes_written = write_n_bytes(sock, buffer, bytes_read);
 
-        for (int i = 0; i < bytes_read; i++){
-            printf("%c", buffer[i]);
-        }
+        } while (bytes_read > 0);
+
+    
 
 
+        close(sock);
 
-   // }
+   }
     
 
     return 0;
