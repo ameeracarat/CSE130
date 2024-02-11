f388df08f05b818a7a86778abf69a5cde2780c00
Reema <rechinta@ucsc.edu>
2024-02-11 06:48:53 +0000

    parsing in progress

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -8,9 +8,16 @@
 #include "asgn2_helper_funcs.h"
 #include <regex.h>
 
+#define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
+
 int main(int argc, char *argv[]) {
 
     regex_t regex;
+    regmatch_t pmatch[4];
+
+    char *re = "^([A-Z]{1,8}) (/[a-zA-Z0-9._]{1,63}) (HTTP)$";
+
+    static const char *s = "GET /foo.txt HTTP";
 
     if (argc != 2) {
         fprintf(stderr, "Invalid Port\n");
@@ -41,19 +48,60 @@ int main(int argc, char *argv[]) {
 
         ssize_t bytes_read = 0;
 
-        //do{
         bytes_read = read_n_bytes(sock, buffer, buff_size);
 
-        printf("bytes read: %zd\n", bytes_read);
+        printf("bytess read: %zd\n", bytes_read);
+
+        for (int i = 0; i < bytes_read; ++i) {
+            printf("%c", buffer[i]);
+        }
+        printf("\n\n\n");
+
+        if (regcomp(&regex, re, REG_NEWLINE | REG_EXTENDED)) {
+            fprintf(stderr, "Failed to compile regex\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int get_put = 2;
+
+        if (regexec(&regex, buffer, ARRAY_SIZE(pmatch), pmatch, 0) == 0) {
+            for (int i = 0; i < ARRAY_SIZE(pmatch); ++i) {
+                regoff_t start = pmatch[i].rm_so;
+                regoff_t end = pmatch[i].rm_eo;
+
+                if (start != -1 && end != -1) {
+                    printf("%.*s\n", (int) (end - start), buffer + start);
+
+                    if (i == 1) {
+
+                        char matched_string[end - start + 1];
+                        strncpy(matched_string, buffer + start, end - start);
+                        matched_string[end - start] = '\0';
+
+                        // Check if the extracted string is equal to "GET"
+                        if (strcmp(matched_string, "GET") == 0) {
+                            printf("Second element is \"GET\"\n");
+                        } else if (strcmp(matched_string, "PUT") == 0) {
+                            printf("Second element is \"PUT\"\n");
+                        }
+                        
+                    }
+
+
+
+
+
+
+
+
+                }
+            }
+        } else {
+            fprintf(stderr, "No match found\n");
+        }
 
         ssize_t bytes_written = write_n_bytes(sock, buffer, bytes_read);
         printf("bytes written: %zd", bytes_written);
-
-        //  } while (bytes_read > 0);
-
-        // char *error_code = "101";
-
-        //  write_n_bytes(STDOUT_FILENO, error_code, 3);
 
         close(sock);
     }
